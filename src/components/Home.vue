01bb82d5f59f7be3d90fc2e00eed2b77d1f6f745
Clinton <clinton@eyekiller.com>
2024-08-12 21:21:28 +0100

    Vue Router and Unhead installed

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1>Home</h1>
+    </div>
+</template>
+  
+<script setup>
+    import { useHead } from 'unhead';
+
+    useHead({
+        title: 'Home Page',
+        meta: [
+            {
+                name: 'description',
+                content: 'The home page of our example app.',
+            },
+        ],
+    });
+</script>
+  
+<style scoped>
+/* Add your styles here */
+</style>
+  
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
+import { createHead } from 'unhead';
 
-createApp(App).mount('#app')
+const head = createHead();
+
+createApp(App)
+  .use(router)  // The order of use() matters here, but this is correct
+  .use(head)    // Use head after router
+  .mount('#app');
